c12ff0cb738f474dcfe903aad26c8919845686df
麦子 <maizi_today@sina.com>
2019-07-24 15:46:22 +0800

    博客提交

--- src/main/java/com/example/demo/TestMain.java
+++ src/main/java/com/example/demo/TestMain.java
@@ -3,12 +3,12 @@ package com.example.demo;
  * @Description: 请输入....
  * @Author: 麦子
  * @Date: 2019-07-24 11:17:03
- * @LastEditTime: 2019-07-24 12:25:36
+ * @LastEditTime: 2019-07-24 15:46:18
  * @LastEditors: 麦子
  */
 
 public class TestMain {
     public static void main(String[] args) {
-        System.out.println("最近记录信息");
+        System.out.println("我是test3分支");
     }
 }
