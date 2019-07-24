3af30689f8a08d679ea31abdf1b93ce91ceba49e
麦子 <maizi_today@sina.com>
2019-07-24 15:30:20 +0800

    博客提交

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,4 +1,11 @@
 package com.example.demo;
+/*
+ * @Description: 请输入....
+ * @Author: 麦子
+ * @Date: 2019-07-22 18:43:57
+ * @LastEditTime: 2019-07-24 15:27:04
+ * @LastEditors: 麦子
+ */
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
