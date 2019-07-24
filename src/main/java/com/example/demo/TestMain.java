22c2b03f152a5617866a5543af6cc4505e029f80
麦子 <maizi_today@sina.com>
2019-07-24 11:40:34 +0800

    同意测试

--- src/main/java/com/example/demo/TestMain.java
+++ src/main/java/com/example/demo/TestMain.java
@@ -3,12 +3,12 @@ package com.example.demo;
  * @Description: 请输入....
  * @Author: 麦子
  * @Date: 2019-07-24 11:17:03
- * @LastEditTime: 2019-07-24 11:17:48
+ * @LastEditTime: 2019-07-24 11:40:15
  * @LastEditors: 麦子
  */
 
 public class TestMain {
     public static void main(String[] args) {
-        System.out.println("测试运行");
+        System.out.println("测试运行------2");
     }
 }
