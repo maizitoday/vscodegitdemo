bd666a6a9da47a4c8c49d69798dc2156b909b70c
麦子 <maizi_today@sina.com>
2019-07-24 11:39:57 +0800

    博客提交

---
+++ src/main/java/com/example/demo/TestMain.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+/*
+ * @Description: 请输入....
+ * @Author: 麦子
+ * @Date: 2019-07-24 11:17:03
+ * @LastEditTime: 2019-07-24 11:17:48
+ * @LastEditors: 麦子
+ */
+
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("测试运行");
+    }
+}
