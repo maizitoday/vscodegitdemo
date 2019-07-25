d95b8eea390265cc38edbcd7c0c322eaadb27a6c
麦子 <maizi_today@sina.com>
2019-07-25 20:02:32 +0800

    博客提交

--- src/main/java/com/example/demo/MyDemoApplication.java
+++ src/main/java/com/example/demo/MyDemoApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
  * @Description: 请输入....
  * @Author: 麦子
  * @Date: 2019-07-22 18:43:57
- * @LastEditTime: 2019-07-24 15:27:04
+ * @LastEditTime: 2019-07-25 19:41:21
  * @LastEditors: 麦子
  */
 
@@ -11,10 +11,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MyDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MyDemoApplication.class, args);
 	}
-
 }
